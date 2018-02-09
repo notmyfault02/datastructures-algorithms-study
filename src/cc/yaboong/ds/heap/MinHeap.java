adf40da18cb88b2be40f47ea6f776e9319f4352a
yaboong <daniel.yebon.yoo@gmail.com>
2018-02-10 01:09:04 +0900

    MinHeap 사용 예제 보강, Comparable 인터페이스 구현한 새로운 클래스 정의해서 min heap 데이터 타입으로 사용해보기

--- src/cc/yaboong/ds/heap/MinHeap.java
+++ src/cc/yaboong/ds/heap/MinHeap.java
@@ -9,10 +9,29 @@ import java.util.Arrays;
 @SuppressWarnings("unchecked")
 public class MinHeap <T extends Comparable<T>> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int capacity = DEFAULT_CAPACITY;
-    private int size = 0;
+    private int capacity;
+    private int size;
+    private T[] items;
 
-    T[] items = (T[]) new Comparable[DEFAULT_CAPACITY];
+
+    public MinHeap() {
+        capacity = DEFAULT_CAPACITY;
+        items = (T[]) new Comparable[capacity];
+        size = 0;
+    }
+
+    public MinHeap(T[] inputArray) {
+        size = inputArray.length;
+        capacity = size;
+        items = Arrays.copyOf(inputArray, size);
+        buildMinHeap();
+    }
+
+    private void buildMinHeap() {
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            heapifyDown(i);
+        }
+    }
 
     private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
     private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
@@ -53,18 +72,17 @@ public class MinHeap <T extends Comparable<T>> {
         T item = items[0];
         items[0] = items[--size];
         items[size] = null;
-        heapifyDown();
+        heapifyDown(0);
         return item;
     }
 
     public void add(T item) {
         ensureExtraCapacity();
         items[size++] = item;
-        heapifyUp();
+        heapifyUp(size - 1);
     }
 
-    private void heapifyUp() {
-        int index = size - 1;
+    private void heapifyUp(int index) {
         while (hasParent(index) && less(items[index], parent(index))) {
             int parentIndex = getParentIndex(index);
             swap(index, parentIndex);
@@ -72,8 +90,7 @@ public class MinHeap <T extends Comparable<T>> {
         }
     }
 
-    private void heapifyDown() {
-        int index = 0;
+    private void heapifyDown(int index) {
         while (hasLeftChild(index)) { // left child 가 없으면 right child 도 없다
             int smallerChildIndex = getLeftChildIndex(index);
             if ( hasRightChild(index) && less( rightChild(index), leftChild(index) ) )
@@ -90,30 +107,48 @@ public class MinHeap <T extends Comparable<T>> {
 
     public void print() {
         for (int i=0; i<size; i++) {
-            System.out.print(items[i] + " ");
+            System.out.print(items[i].toString() + " ");
         }
         System.out.print("\n");
     }
 
     public static void main(String[] args) {
-        MinHeap<Integer> minHeap = new MinHeap<>();
-
-        minHeap.add(13);
-        minHeap.add(12);
-        minHeap.add(11);
-        minHeap.add(10);
-        minHeap.add(9);
-        minHeap.add(8);
-        minHeap.add(7);
-        minHeap.add(6);
-        minHeap.add(5);
-        minHeap.add(4);
-        minHeap.add(3);
-        minHeap.add(2);
-        minHeap.add(1);
+        Integer[] arrayOfInteger = {137440, 176640, 219000, 266110, 289000, 294730, 129000};
+        MinHeap<Integer> minHeap = new MinHeap<>(arrayOfInteger);
         minHeap.print();
 
-        minHeap.poll();
-        minHeap.print();
+        class Product implements Comparable<Product> {
+            String name;
+            int price;
+
+            public Product(String name, int price) {
+                super();
+                this.name = name;
+                this.price= price;
+            }
+
+            @Override
+            public int compareTo(Product obj) {
+                if      (this.price < obj.price) return -1;
+                else if (this.price > obj.price) return 1;
+                else                                   return 0;
+            }
+
+            public String toString() {
+                return "{" + this.name + ", " + this.price + "}";
+            }
+        }
+
+        Product[] arrayOfProduct = {
+                new Product("알파스캔 24형 AOC 2470", 137440),
+                new Product("알파스캔 2481 IPS", 176640),
+                new Product("알파스캔 광시야각 AOC 2779", 219000),
+                new Product("알파스캔 27형 AOC 2777", 266110),
+                new Product("알파스캔 광시야각 AOC 3288", 289000),
+                new Product("알파스캔 게이밍 무결점 AOC G2460", 294730),
+                new Product("알파스캔 프레스티지 2215", 129000)
+        };
+        MinHeap<Product> personMinHeap = new MinHeap<>(arrayOfProduct);
+        personMinHeap.print();
     }
 }
