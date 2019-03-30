7342e642f8598cbeb8cd94ad9622279fb9fe9fb4
yaboong <daniel.yebon.yoo@gmail.com>
2019-03-30 22:43:34 +0900

    Fix: binary search 변수명 수정

--- src/cc/yaboong/algorithms/search/BinarySearch.java
+++ src/cc/yaboong/algorithms/search/BinarySearch.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
  * Created by yaboong on 2018. 1. 30..
  */
 public class BinarySearch {
-    static int binarySearch(int[] arr, int left, int right, int x) {
-        if (right >= left) {
-            int mid = left + (right - left) / 2;
+    static int binarySearch(int[] arr, int lo, int hi, int target) {
+        if (hi >= lo) {
+            int mid = lo + (hi - lo) / 2;
 
-            if (arr[mid] == x) return mid;
+            if (arr[mid] == target) return mid;
 
-            if (arr[mid] > x) return binarySearch(arr, left, mid-1, x);
-            else              return binarySearch(arr, mid + 1, right, x);
+            if (arr[mid] > target) return binarySearch(arr, lo, mid-1, target);
+            else              return binarySearch(arr, mid + 1, hi, target);
         }
         return -1;
     }
